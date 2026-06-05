4b737ac827ecf240a501932f6676a7f8fbc49edd
borovinskiy_me <m.borovinskiy@mail.ru>
2026-06-06 00:30:46 +0300

    004_srvc add vault secrets to container when initiated, inject to bean

--- eventgenerator/src/main/java/ru/boro/busapps/eventgenerator/Eventgenerator.java
+++ eventgenerator/src/main/java/ru/boro/busapps/eventgenerator/Eventgenerator.java
@@ -1,5 +1,8 @@
 package ru.boro.busapps.eventgenerator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +10,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class Eventgenerator {
+
+    private Logger logger = LoggerFactory.getLogger(Eventgenerator.class);
+
+
+    @Value("${topicAUTO}")
+    String topicAUTO;
+    @Value("${topicMonitoring}")
+    String topicMonitoring;
 
     public static void main(String[] args)  {
         System.setProperty("user.timezone", "Europe/Moscow");
