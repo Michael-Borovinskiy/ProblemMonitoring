16202a54911bd9134f6b68e202caab3038ef948c
borovinskiy_me <m.borovinskiy@mail.ru>
2026-06-10 21:01:55 +0300

    004_srvc config fixes

--- eventgenerator/src/main/java/ru/boro/busapps/eventgenerator/kafkaconfigs/KfkConfig.java
+++ eventgenerator/src/main/java/ru/boro/busapps/eventgenerator/kafkaconfigs/KfkConfig.java
@@ -2,6 +2,7 @@ package ru.boro.busapps.eventgenerator.kafkaconfigs;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,6 +15,9 @@ import java.util.Map;
 @Configuration
 public class KfkConfig {
 
+    @Value("${bootstrapServerConfig}")
+    String bootstrapServerConfig;
+
     @Bean
     public ProducerFactory<Long, Object> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
@@ -22,7 +26,7 @@ public class KfkConfig {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "ru.boro.busapps.modellibs.models.EventSerializer");
         return props;
